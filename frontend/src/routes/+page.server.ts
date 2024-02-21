42dea82a86ac6b0741e2796c91b79e17a1f394a3
Vlad Tomashpolskyi <wizzardich@gmail.com>
2024-02-21 22:37:44 +0100

    chore: cleanup logging

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -6,23 +6,5 @@ const backend = new BackendService(env.SVC_AUTOREPLACE_BASE_URL);
 
 export const load: PageServerLoad = async ({ }) => {
     const templateList = await backend.getTemplates();
-	console.log(templateList);
-	templateList.forEach((template) => {
-		console.log(template);
-		template.replacements.forEach((replacement) => {
-			console.log(replacement);
-		});
-	});
-
     return { templates: templateList };
 };
-
-// export const actions = {
-// 	//@ts-ignore
-// 	loadTemplate: async ({ request }) => {
-// 		const data = await request.formData();
-// 		const template = await backend.fetchTemplateById(data.get('template-id'));
-// 		console.log(template);
-// 		return { success: true, template: template };
-// 	},
-// };
